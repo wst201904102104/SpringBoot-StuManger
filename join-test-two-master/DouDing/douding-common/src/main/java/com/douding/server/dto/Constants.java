854c600c4506f585f87bba415e8ef24641cd88c5
wst <1063383540@qq.com>
2023-05-17 09:39:42 +0800

    学生信息管理系统

--- join-test-two-master/DouDing/douding-common/src/main/java/com/douding/server/dto/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.douding.server.dto;
-
-public class Constants {
-
-    public static final String LOGIN_USER = "LOGIN_USER";
-}
--- join-test-two-master/DouDing/douding-common/src/main/java/com/douding/server/exception/ValidatorException.java
+++
@@ -1,8 +0,0 @@
-package com.douding.server.exception;
-
-public class ValidatorException extends RuntimeException{
-
-    public ValidatorException(String message) {
-        super(message);
-    }
-}
